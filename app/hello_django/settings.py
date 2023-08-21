a42c99c6438f6da1e1776a0c851d876481956776
Deniss <deniss.hohlov@gmail.com>
2023-08-21 14:26:34 +0300

    commited

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['t10.ee']
 
-CSRF_TRUSTED_ORIGINS = ["https://t10.ee"]
+#CSRF_TRUSTED_ORIGINS = ["https://t10.ee"]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Application definition
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
